6f04bb6513459b06cb9f137f71950a281e676f89
Sebastien Heraud <sebastien@heraud.me>
2014-11-30 19:08:32 +0100

    Initial commit of Joomla Print Link

---
+++ plugins/cck_field_link/joomla_print/joomla_print.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_LinkJoomla_Print extends JCckPluginLink
+{
+	protected static $type	=	'joomla_print';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+	
+	// onCCK_Field_LinkPrepareContent
+	public static function onCCK_Field_LinkPrepareContent( &$field, &$config = array() )
+	{		
+		if ( self::$type != $field->link ) {
+			return;
+		}
+		
+		// Prepare
+		$link	=	parent::g_getLink( $field->link_options );
+		
+		// Set
+		$field->link	=	'';
+		self::_link( $link, $field, $config );
+	}
+	
+	// _link
+	protected static function _link( $link, &$field, &$config )
+	{	
+		// Init
+		$link_class		=	$link->get( 'class', '' );
+		
+		// Set
+		$field->link			=	JUri::current();
+		$field->link			.=	( strpos( $field->link, '?' ) !== false ) ? '&' : '?';
+		$field->link			.=	'tmpl=component&print=1';
+		$field->link_class		=	$link_class ? $link_class : ( isset( $field->link_class ) ? $field->link_class : '' );
+		$field->link_rel		=	'nofollow';
+		$field->link_onclick	=	'window.open(this.href,\'win2\',\'status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=640,height=480,directories=no,location=no\'); return false;';
+	}
+}
+?>
