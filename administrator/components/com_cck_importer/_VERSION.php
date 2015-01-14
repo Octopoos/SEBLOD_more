d44a8763242b2140271169ecf3707b2d5130e0c8
Sebastien Heraud <sebastien@heraud.me>
2015-01-14 14:06:26 +0100

    Implement JCckImporterVersion class

--- administrator/components/com_cck_importer/_VERSION.php
+++ administrator/components/com_cck_importer/_VERSION.php
@@ -1,1 +1,26 @@
-1.3.3
+<?php
+/**
+* @version 			SEBLOD Importer 1.x
+* @package			SEBLOD Importer Add-on for SEBLOD 3.x
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// JCckImporterVersion
+final class JCckImporterVersion
+{
+	public $RELEASE = '1.4';
+	
+	public $DEV_LEVEL = '0';
+
+	// getShortVersion
+	public function getShortVersion()
+	{
+		return $this->RELEASE . '.' . $this->DEV_LEVEL;
+	}
+}
+?>
