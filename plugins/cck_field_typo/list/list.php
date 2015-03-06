c04e65a8a93dc6eb7b734ffaaa38c3f5fbcfdfed
Sebastien Heraud <sebastien@heraud.me>
2015-03-06 11:10:06 +0100

    Initial commit of List Typography plug-in

---
+++ plugins/cck_field_typo/list/list.php
@@ -0,0 +1,78 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_TypoList extends JCckPluginTypo
+{
+	protected static $type	=	'list';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+		
+	// onCCK_Field_TypoPrepareContent
+	public function onCCK_Field_TypoPrepareContent( &$field, $target = 'value', &$config = array() )
+	{		
+		if ( self::$type != $field->typo ) {
+			return;
+		}
+		
+		// Prepare
+		$typo	=	parent::g_getTypo( $field->typo_options );
+		$value	=	$field->$target;
+		
+		// Set
+		if ( $field->typo_label ) {
+			$field->label	=	self::_typo( $typo, $field, $field->label, $config );
+		}
+		$field->typo		=	self::_typo( $typo, $field, $value, $config );
+	}
+	
+	// _typo
+	protected static function _typo( $typo, $field, $value, &$config = array() )
+	{
+		$class		=	$typo->get( 'class', '' );
+		$html		=	'';
+		$tag		=	'ul';
+		
+		if ( isset( $field->values ) && count( $field->values ) ) {
+			$html	.=	'<'.$tag.' class="'.$class.'">';
+			$target	=	$field->typo_target;
+			foreach ( $field->values as $k=>$v ) {
+				$v	=	parent::g_hasLink( $v, $typo, $v->$target );
+				$html	.=	'<li>'.$v.'</li>';
+			}
+			$html	.=	'</'.$tag.'>';
+		} else if ( is_array( $value ) && count( $value ) ) {
+			$html	.=	'<'.$tag.' class="'.$class.'">';
+			foreach( $value as $v ) {
+				if ( is_array( $v ) ) {
+					$v	=	current( $v );
+				}
+				if ( is_object( $v ) ) {
+					$html	.=	'<li>'.JCck::callFunc( 'plgCCK_Field'.$v->type, 'onCCK_FieldRenderContent', $v ).'</li>';
+				}
+			}
+			$html	.=	'</'.$tag.'>';
+		} elseif ( $field->divider ) {
+			$value	=	explode( $field->divider, $value );
+			if ( is_array( $value ) && count( $value ) ) {
+				$html	.=	'<'.$tag.' class="'.$class.'">';
+				foreach( $value as $v ) {
+					$html	.=	'<li>'.$v.'</li>';
+				}
+				$html	.=	'</'.$tag.'>';
+			}
+		}
+		
+		return $html;
+	}
+}
+?>
