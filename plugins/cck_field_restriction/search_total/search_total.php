f3d2dda1385848ee071664c528d7bf2b2c9060f0
Sebastien Heraud <sebastien@heraud.me>
2015-04-20 12:27:33 +0200

    Initial Commit of Search Total Restriction Plug-in

---
+++ plugins/cck_field_restriction/search_total/search_total.php
@@ -0,0 +1,91 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_RestrictionSearch_Total extends JCckPluginRestriction
+{
+	protected static $type	=	'search_total';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+
+	// onCCK_Field_RestrictionPrepareContent
+	public static function onCCK_Field_RestrictionPrepareContent( &$field, &$config )
+	{
+		if ( self::$type != $field->restriction ) {
+			return;
+		}
+		
+		$restriction	=	parent::g_getRestriction( $field->restriction_options );
+		
+		return self::_authorise( $restriction, $field, $config );
+	}
+
+	// onCCK_Field_RestrictionPrepareForm
+	public static function onCCK_Field_RestrictionPrepareForm( &$field, &$config )
+	{
+		if ( self::$type != $field->restriction ) {
+			return;
+		}
+		
+		$restriction	=	parent::g_getRestriction( $field->restriction_options );
+		
+		return self::_authorise( $restriction, $field, $config );
+	}
+	
+	// onCCK_Field_RestrictionPrepareStore
+	public static function onCCK_Field_RestrictionPrepareStore( &$field, &$config )
+	{
+		if ( self::$type != $field->restriction ) {
+			return;
+		}
+		
+		$restriction	=	parent::g_getRestriction( $field->restriction_options );
+		
+		return self::_authorise( $restriction, $field, $config );
+	}
+	
+	// _authorise
+	protected static function _authorise( $restriction, &$field, &$config )
+	{
+		if ( $config['client'] == 'search' ) {
+			parent::g_addProcess( 'beforeRenderForm', self::$type, $config, array( 'name'=>$field->name, 'restriction'=>$restriction ) );
+		}
+
+		return true;
+	}
+
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Special Events
+
+	// onCCK_Field_RestrictionBeforeRenderForm
+	public static function onCCK_Field_RestrictionBeforeRenderForm( $process, &$fields, &$storages, &$config = array() )
+	{
+		$name			=	$process['name'];
+		$restriction	=	$process['restriction'];
+
+		$do		=	$restriction->get( 'do', 0 );
+
+		if ( $config['total'] > 0 ) {
+			$do	=	( $do ) ? false : true;
+		} else {
+			$do	=	( $do ) ? true : false;
+		}
+
+		if ( $do ) {
+			return true;
+		} else {
+			$fields[$name]->display	=	0;
+			$fields[$name]->state	=	0;
+			return false;
+		}
+	}
+}
+?>
