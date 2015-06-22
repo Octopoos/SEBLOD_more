e34f59f3bc5806c28690c240630de778dac59922
Sebastien Heraud <sebastien@heraud.me>
2015-06-22 19:45:18 +0200

    Initial Commit of Random UniqID

---
+++ plugins/cck_field_live/random_uniqid/random_uniqid.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_LiveRandom_Uniqid extends JCckPluginLive
+{
+	protected static $type	=	'random_uniqid';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+	
+	// onCCK_Field_LivePrepareForm
+	public function onCCK_Field_LivePrepareForm( &$field, &$value = '', &$config = array() )
+	{
+		if ( self::$type != $field->live ) {
+			return;
+		}
+		
+		// Set
+		$value	=	(string)str_replace( '.', '', uniqid( '', true ) );
+	}
+}
+?>
