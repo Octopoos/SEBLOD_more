64e8035aab6f803ea45264120c5f9ceee66d5e58
Sebastien Heraud <sebastien@heraud.me>
2015-08-24 13:35:23 +0200

    Add "Confirm" capability & params [Custom (JS)]

--- plugins/cck_field_link/custom_js/custom_js.php
+++ plugins/cck_field_link/custom_js/custom_js.php
@@ -36,10 +36,22 @@ class plgCCK_Field_LinkCustom_Js extends JCckPluginLink
 	protected static function _link( $link, &$field, &$config )
 	{
 		$link_class				=	$link->get( 'class', '' );
-		
+		$link_onclick			=	htmlspecialchars( $link->get( 'custom' ) );
+
 		$field->link			=	'javascript: void(0);';
 		$field->link_class		=	$link_class ? $link_class : ( isset( $field->link_class ) ? $field->link_class : '' );
-		$field->link_onclick	=	htmlspecialchars( $link->get( 'custom' ) );
+		
+		if ( $link->get( 'confirm', 1 ) ) {
+			$alert					=	$link->get( 'confirm_alert' ); // JText::_( 'COM_CCK_CONFIRM_DELETE' )
+			if ( $config['doTranslation'] ) {
+				if ( trim( $alert ) ) {
+					$alert			=	JText::_( 'COM_CCK_' . str_replace( ' ', '_', trim( $alert ) ) );
+				}
+			}
+			$field->link_onclick	=	'if(!confirm(\''.addslashes( $alert ).'\')){return false;}else{'.$link_onclick.'}';
+		} else {
+			$field->link_onclick	=	$link_onclick;
+		}
 	}
 }
 ?>
--- plugins/cck_field_link/custom_js/tmpl/edit.php
+++ plugins/cck_field_link/custom_js/tmpl/edit.php
@@ -18,6 +18,8 @@ JCckDev::initScript( 'link', $this->item );
     <ul class="adminformlist adminformlist-2cols">
         <?php
 		echo JCckDev::renderForm( 'core_dev_textarea', '', $config, array( 'label'=>'Javascript', 'cols'=>92, 'rows'=>1, 'storage_field'=>'custom' ), array(), 'w100' );
+		echo JCckDev::renderForm( 'core_dev_bool', '', $config, array( 'defaultvalue'=>0, 'label'=>'Confirm', 'storage_field'=>'confirm' ) );
+		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Alert', 'defaultvalue'=>'Please Confirm', 'size'=>32, 'storage_field'=>'confirm_alert' ) );
 
 		echo JCckDev::renderSpacer( JText::_( 'COM_CCK_CONSTRUCTION' ) . '<span class="mini">('.JText::_( 'COM_CCK_GENERIC' ).')</span>' );
 		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Class', 'size'=>24, 'storage_field'=>'class' ) );
@@ -34,5 +36,6 @@ JCckDev::initScript( 'link', $this->item );
 <script type="text/javascript">
 jQuery(document).ready(function($) {
 	$('#title_custom').isVisibleWhen('title','2',false);
+	$('#confirm_alert').isVisibleWhen('confirm','1');
 });
 </script>
