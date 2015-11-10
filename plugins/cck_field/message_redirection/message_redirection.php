d4b4e384c779b74dee6a867b0a6f5975ac60126c
Sebastien Heraud <sebastien@heraud.me>
2015-11-10 15:50:45 +0100

    Initial commit of Message Redirection Field Plug-in

---
+++ plugins/cck_field/message_redirection/message_redirection.php
@@ -0,0 +1,141 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2013 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_FieldMessage_Redirection extends JCckPluginField
+{
+	protected static $type	=	'message_redirection';
+	protected static $path;
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Construct
+	
+	// onCCK_FieldConstruct
+	public function onCCK_FieldConstruct( $type, &$data = array() )
+	{
+		if ( self::$type != $type ) {
+			return;
+		}
+		parent::g_onCCK_FieldConstruct( $data );
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+	
+	// onCCK_FieldPrepareContent
+	public function onCCK_FieldPrepareContent( &$field, $value = '', &$config = array() )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		parent::g_onCCK_FieldPrepareContent( $field, $config );
+		
+		// Init
+		if ( $field->state ) {
+			$options2	=	json_decode( $field->options2 );
+
+			if ( is_object( $options2 ) && isset( $options2->itemid ) && $options2->itemid ) {
+				$redirection	=	'window.location.href=\''.JCckDevHelper::getAbsoluteUrl( $options2->itemid ).'\'';
+				$time			=	5000;
+				
+				JFactory::getDocument()->addScriptDeclaration( 'setTimeout("'.$redirection.'",'.$time.');' );
+			}	
+		}
+
+		// Set
+		$field->value	=	$value;
+	}
+	
+	// onCCK_FieldPrepareForm
+	public function onCCK_FieldPrepareForm( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		self::$path	=	parent::g_getPath( self::$type.'/' );
+		parent::g_onCCK_FieldPrepareForm( $field, $config );
+		
+		// Set
+		$field->display	=	0;
+		$field->form	=	'';
+		$field->value	=	$value;
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
+	}
+	
+	// onCCK_FieldPrepareSearch
+	public function onCCK_FieldPrepareSearch( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		
+		// Prepare
+		self::onCCK_FieldPrepareForm( $field, $value, $config, $inherit, $return );
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
+	}
+	
+	// onCCK_FieldPrepareStore
+	public function onCCK_FieldPrepareStore( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+
+		/*
+		// Init
+		if ( count( $inherit ) ) {
+			$name	=	( isset( $inherit['name'] ) && $inherit['name'] != '' ) ? $inherit['name'] : $field->name;
+		} else {
+			$name	=	$field->name;
+		}
+
+		parent::g_onCCK_FieldPrepareStore( $field, $name, $value, $config );
+
+		if ( !$field->state ) {
+			return;
+		}
+		*/
+
+		// Prepare
+		$options2	=	json_decode( $field->options2 );
+
+		if ( is_object( $options2 ) && isset( $options2->itemid ) && $options2->itemid ) {
+			$url	=	JCckDevHelper::getAbsoluteUrl( $options2->itemid );
+
+			if ( $url != '' ) {
+				$config['options']['redirection']	=	'';
+				$config['url']						=	$url;
+			}
+		}
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Render
+	
+	// onCCK_FieldRenderContent
+	public static function onCCK_FieldRenderContent( $field, &$config = array() )
+	{
+		return parent::g_onCCK_FieldRenderContent( $field );
+	}
+	
+	// onCCK_FieldRenderForm
+	public static function onCCK_FieldRenderForm( $field, &$config = array() )
+	{
+		return parent::g_onCCK_FieldRenderForm( $field );
+	}
+}
+?>
