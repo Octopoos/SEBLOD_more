a1683f0549e8cd748ff43dd269db5d8868137fe0
Sebastien Heraud <sebastien@heraud.me>
2016-01-11 11:30:26 +0100

    Allow Typo to be applied as Container or Content (vs Link)

--- plugins/cck_field_typo/pre/pre.php
+++ plugins/cck_field_typo/pre/pre.php
@@ -26,13 +26,28 @@ class plgCCK_Field_TypoPre extends JCckPluginTypo
 		
 		// Prepare
 		$typo	=	parent::g_getTypo( $field->typo_options );
-		$value	=	parent::g_hasLink( $field, $typo, $field->$target );
+		$mode	=	(int)$typo->get( 'typo_mode', '0' );
+
+		if ( !$mode ) {
+			$value	=	parent::g_hasLink( $field, $typo, $field->$target );
+		} else {
+			$value	=	$field->$target;
+		}
 		
 		// Set
 		if ( $field->typo_label ) {
 			$field->label	=	self::_typo( $typo, $field, $field->label, $config );
 		}
 		$field->typo		=	self::_typo( $typo, $field, $value, $config );
+
+		if ( $mode ) {
+			$field->typo		=	parent::g_hasLink( $field, $typo, $field->typo );
+			$field->typo_mode	=	1;
+		} else {
+			/*if ( $field->link == '' ) {*/
+			parent::g_addProcess( 'beforeRenderContent', self::$type, $config, array( 'name'=>$field->name ) );
+			/*}*/
+		}
 	}
 	
 	// _typo
@@ -41,6 +56,20 @@ class plgCCK_Field_TypoPre extends JCckPluginTypo
 		$value	=	htmlentities( $value );
 		
 		return '<pre>'.$value.'</pre>';
+	}
+
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Special Events
+	
+	// onCCK_Field_TypoBeforeRenderContent
+	public static function onCCK_Field_TypoBeforeRenderContent( $process, &$fields, &$storages, &$config = array() )
+	{
+		$name	=	$process['name'];
+
+		if ( isset( $fields[$name]->html ) && $fields[$name]->typo_target != 'html' ) {
+			if ( strpos( $fields[$name]->typo, '<a' ) !== false ) {
+				$fields[$name]->typo	=	'<pre>'.htmlentities( $fields[$name]->html ).'</pre>';
+			}
+		}
 	}
 }
 ?>
