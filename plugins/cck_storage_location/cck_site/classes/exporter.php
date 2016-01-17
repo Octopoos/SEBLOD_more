b1ceb37a7d79a9dddaf605ad767f5a775ea28c1c
Sebastien Heraud <sebastien@heraud.me>
2016-01-17 14:16:02 +0100

    Initial commit of SEBLOD Site Object Plug-in

---
+++ plugins/cck_storage_location/cck_site/classes/exporter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+require_once JPATH_SITE.'/plugins/cck_storage_location/cck_site/cck_site.php';
+
+// Class
+class plgCCK_Storage_LocationCck_Site_Exporter extends plgCCK_Storage_LocationCck_Site
+{
+	// onCCK_Storage_LocationExport
+	public static function onCCK_Storage_LocationExport( $items, &$config = array() )
+	{
+		// TODO
+	}
+}
+?>
