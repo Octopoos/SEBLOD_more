6eb4feedc73964e974d9b1b92a4c17285e83f448
Sebastien Heraud <sebastien@heraud.me>
2016-02-13 16:58:00 +0100

    Initial commit of Number Format Validation Plug-in

---
+++ plugins/cck_field_validation/number_format/number_format.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_ValidationNumber_Format extends JCckPluginValidation
+{
+	protected static $type	=	'number_format';
+	protected static $regex	=	'';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+
+	// onCCK_Field_ValidationPrepareForm
+	public static function onCCK_Field_ValidationPrepareForm( &$field, $fieldId, &$config )
+	{
+		if ( self::$type != $field->validation ) {
+			return;
+		}
+	}
+	
+	// onCCK_Field_ValidationPrepareStore
+	public static function onCCK_Field_ValidationPrepareStore( &$field, $name, &$value, &$config )
+	{
+		if ( $value == '' ) {
+			return;
+		}
+		$validation	=	parent::g_getValidation( $field->validation_options, false );
+
+		// Decimals
+		$pos		=	strrpos( $value, $validation->get( 'input_decimals_separator', ',' ) );
+		if ( $pos !== false && $pos > 0 && isset( $value[$pos] ) ) {
+			$replace		=	$validation->get( 'output_decimals_separator', '.' );
+			$value[$pos]	=	$replace;
+		}
+
+		// Thousands
+		$search		=	$validation->get( 'input_thousands_separator', '' );
+		$replace	=	$validation->get( 'output_thousands_separator', '' );
+		$value		=	str_replace( $search, $replace, $value );
+	}
+}
+?>
---
+++ plugins/cck_field_validation/number_format/tmpl/edit.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Decimals Separator', 'defaultvalue'=>',', 'size'=>'8', 'storage_field'=>'input_decimals_separator', 'required'=>'required', 'attributes'=>'placeholder="Input"' ) );
+echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Decimals Separator', 'defaultvalue'=>'.', 'size'=>'8', 'storage_field'=>'output_decimals_separator', 'required'=>'required', 'attributes'=>'placeholder="Storage"' ) );
+echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Thousands Separator', 'size'=>'8', 'storage_field'=>'input_thousands_separator', 'attributes'=>'placeholder="Input"' ) );
+echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Thousands Separator', 'size'=>'8', 'storage_field'=>'output_thousands_separator', 'attributes'=>'placeholder="Storage"' ) );
+?>
+
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+	$('#alert').isVisibleWhen('decimals','-1');
+});
+</script>
