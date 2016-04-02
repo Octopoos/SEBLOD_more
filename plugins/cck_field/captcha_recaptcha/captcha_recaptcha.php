97c71095963cff59b1bfdd9a50b1f5b962b5970e
Sebastien Heraud <sebastien@heraud.me>
2016-04-02 11:58:49 +0200

    Clean useless parameters [Captcha Recaptcha Field Plug-in]

--- plugins/cck_field/captcha_recaptcha/captcha_recaptcha.php
+++ plugins/cck_field/captcha_recaptcha/captcha_recaptcha.php
@@ -27,6 +27,14 @@ class plgCCK_FieldCaptcha_Recaptcha extends JCckPluginField
 		parent::g_onCCK_FieldConstruct( $data );
 	}
 	
+	// onCCK_FieldConstruct_SearchSearch
+	public static function onCCK_FieldConstruct_SearchSearch( &$field, $style, $data = array(), &$config = array() )
+	{
+		$data['match_mode']	=	NULL;
+
+		parent::onCCK_FieldConstruct_SearchSearch( $field, $style, $data, $config );
+	}
+	
 	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
 	
 	// onCCK_FieldPrepareContent
