3838b03a47a9a6107e17f30759fd2bb842bb4b9e
Sebastien Heraud <sebastien@heraud.me>
2016-08-25 11:33:55 +0200

    Clean/Remove/Update (Joomla! Consistency) [Joomla! Module Object]

--- plugins/cck_storage_location/joomla_module/joomla_module.php
+++ plugins/cck_storage_location/joomla_module/joomla_module.php
@@ -198,8 +198,8 @@ class plgCCK_Storage_LocationJoomla_Module extends JCckPluginLocation
 		$canDelete		=	$user->authorise( 'core.delete', 'com_cck.form.'.$config['type_id'] );
 		$canDeleteOwn	=	$user->authorise( 'core.delete.own', 'com_cck.form.'.$config['type_id'] );
 		if ( ( !$canDelete && !$canDeleteOwn ) ||
-			( !$canDelete && $canDeleteOwn && $config['author'] != $user->get( 'id' ) ) ||
-			( $canDelete && !$canDeleteOwn && $config['author'] == $user->get( 'id' ) ) ) {
+			( !$canDelete && $canDeleteOwn && $config['author'] != $user->id ) ||
+			( $canDelete && !$canDeleteOwn && $config['author'] == $user->id ) ) {
 			$app->enqueueMessage( JText::_( 'COM_CCK_ERROR_DELETE_NOT_PERMITTED' ), 'error' );
 			return;
 		}
