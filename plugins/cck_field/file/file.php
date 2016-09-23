fde8ff7adf80cdd60a5b367fc76fab758d2c2e1f
Sebastien Heraud <sebastien@heraud.me>
2016-09-23 12:02:11 +0200

    Initial commit of File Field Plug-in for SEBLOD

---
+++ plugins/cck_field/file/file.php
@@ -0,0 +1,222 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_FieldFile extends JCckPluginField
+{
+	protected static $type	=	'file';
+	protected static $friendly	=	1;
+	protected static $path;
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Construct
+	
+	// onCCK_FieldConstruct
+	public function onCCK_FieldConstruct( $type, &$data = array() )
+	{
+		if ( self::$type != $type ) {
+			return;
+		}
+		parent::g_onCCK_FieldConstruct( $data );
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+	
+	// onCCK_FieldPrepareContent
+	public function onCCK_FieldPrepareContent( &$field, $value = '', &$config = array() )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		parent::g_onCCK_FieldPrepareContent( $field, $config );
+		
+		// Prepare
+		if ( $field->bool ) {
+			$options2	=	JCckDev::fromJSON( $field->options2 );
+			$path		=	$options2['path'];
+			$value		=	$path.$value;
+		}
+
+		// Set
+		$field->value	=	$value;
+	}
+	
+	// onCCK_FieldPrepareForm
+	public function onCCK_FieldPrepareForm( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		self::$path	=	parent::g_getPath( self::$type.'/' );
+		parent::g_onCCK_FieldPrepareForm( $field, $config );
+		
+		// Init
+		if ( count( $inherit ) ) {
+			$id		=	( isset( $inherit['id'] ) && $inherit['id'] != '' ) ? $inherit['id'] : $field->name;
+			$name	=	( isset( $inherit['name'] ) && $inherit['name'] != '' ) ? $inherit['name'] : $field->name;
+		} else {
+			$id		=	$field->name;
+			$name	=	$field->name;
+		}
+		$value		=	( $value != '' ) ? $value : $field->defaultvalue;
+		
+		// Validate
+		$validate	=	'';
+		if ( $config['doValidation'] > 1 ) {
+			plgCCK_Field_ValidationRequired::onCCK_Field_ValidationPrepareForm( $field, $id, $config );
+			$validate	=	( count( $field->validate ) ) ? ' validate['.implode( ',', $field->validate ).']' : '';
+		}
+		
+		// Prepare
+		jimport( 'joomla.filesystem.folder' );
+		$options2	=	JCckDev::fromJSON( $field->options2 );
+		$path		=	substr( $options2['path'], 0, -1 );
+		$files		=	JFolder::files( JPATH_SITE.DIRECTORY_SEPARATOR.$path, '.', $field->bool2, true );
+		$legal_ext		=	isset( $options2['media_extensions'] ) ? $options2['media_extensions'] : 'custom';
+		if ( $legal_ext == 'custom' ) {
+			$legal_ext	=	$options2['legal_extensions'];
+		} else {
+			$legal_ext	=	JCck::getConfig_Param( 'media_'.$legal_ext.'_extensions' );
+			if ( !$legal_ext ) {
+				$legal_ext	=	$options2['legal_extensions'];
+			}
+		}
+		$legal_ext	=	explode( ',', $legal_ext );
+		$opts		=	array();
+		if ( trim( $field->selectlabel ) ) {
+			if ( $config['doTranslation'] ) {
+				$field->selectlabel	=	JText::_( 'COM_CCK_' . str_replace( ' ', '_', trim( $field->selectlabel ) ) );
+			}
+			$opts[]	=	JHtml::_( 'select.option',  '', '- '.$field->selectlabel.' -', 'value', 'text' );
+		}
+		if ( $files ) {
+			foreach( $files as $val ) {
+				$val 	=	str_replace( '\\', '/', $val );
+				$val 	=	substr( strstr( $val, $options2['path'] ), strlen( $options2['path'] ) );
+				$pos	=	strrpos( $val, '.' );
+				$ext	=	substr( $val, $pos+1 );
+
+				if ( in_array( $ext, $legal_ext ) ) {
+					$opts[] = JHtml::_( 'select.option', $val, $val );
+				}
+			}
+		}
+		
+		if ( $value && strpos( $value, @$options2['path'] ) !== false ) {
+			$file = str_replace( @$options2['path'], '', $value );
+		}
+		$selectedFileList	=	( $value && @$file ) ? $file : $value;
+		$form				=	JHtml::_( 'select.genericlist', $opts, $name, 'class="inputbox select '.$validate.'"', 'value', 'text', $selectedFileList );
+		
+		// Set
+		if ( ! $field->variation ) {
+			$field->form	=	$form;
+			if ( $field->script ) {
+				parent::g_addScriptDeclaration( $field->script );
+			}
+		} else {
+			parent::g_getDisplayVariation( $field, $field->variation, $value, $value, $form, $id, $name, '<select', '', '', $config );
+		}
+		$field->value	=	$value;
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
+	}
+	
+	// onCCK_FieldPrepareSearch
+	public function onCCK_FieldPrepareSearch( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		
+		// Prepare
+		self::onCCK_FieldPrepareForm( $field, $value, $config, $inherit, $return );
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
+	}
+	
+	// onCCK_FieldPrepareStore
+	public function onCCK_FieldPrepareStore( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		
+		// Init
+		if ( count( $inherit ) ) {
+			$name	=	( isset( $inherit['name'] ) && $inherit['name'] != '' ) ? $inherit['name'] : $field->name;
+		} else {
+			$name	=	$field->name;
+		}
+		
+		// Validate
+		parent::g_onCCK_FieldPrepareStore_Validation( $field, $name, $value, $config );
+		
+		// Set or Return
+		if ( $value != '' ) {
+			$options2		=	JCckDev::fromJSON( $field->options2 );
+			$legal_ext		=	isset( $options2['media_extensions'] ) ? $options2['media_extensions'] : 'custom';
+			if ( $legal_ext == 'custom' ) {
+				$legal_ext	=	$options2['legal_extensions'];
+			} else {
+				$legal_ext	=	JCck::getConfig_Param( 'media_'.$legal_ext.'_extensions' );
+				if ( !$legal_ext ) {
+					$legal_ext	=	$options2['legal_extensions'];
+				}
+			}
+			$legal_ext	=	explode( ',', $legal_ext );
+			$pos		=	strrpos( $value, '.' );
+			$ext		=	substr( $value, $pos+1 );
+
+			if ( !in_array( $ext, $legal_ext ) ) {
+				$value	=	'';
+			}
+			if ( $value && !$field->bool ) {
+				$value	=	$options2['path'].$value;
+			}
+		}
+		
+		if ( $return === true ) {
+			return $value;
+		}
+		$field->value	=	$value;
+		parent::g_onCCK_FieldPrepareStore( $field, $name, $value, $config );
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Render
+	
+	// onCCK_FieldRenderContent
+	public static function onCCK_FieldRenderContent( $field, &$config = array() )
+	{
+		return parent::g_onCCK_FieldRenderContent( $field );
+	}
+	
+	// onCCK_FieldRenderForm
+	public static function onCCK_FieldRenderForm( $field, &$config = array() )
+	{
+		return parent::g_onCCK_FieldRenderForm( $field );
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Stuff & Script
+	
+	// isFriendly
+	public static function isFriendly()
+	{
+		return self::$friendly;
+	}
+}
+?>
---
+++ plugins/cck_field/file/tmpl/edit.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+$options2	=	JCckDev::fromJSON( $this->item->options2 );
+$media_ext	=	( $this->isNew ) ? '' : ( ( isset( $options2['media_extensions'] ) ) ? $options2['media_extensions'] : 'custom' );
+?>
+
+<div class="seblod">
+    <?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_'.$this->item->type.'_DESC' ) ); ?>
+    <ul class="adminformlist adminformlist-2cols">
+        <?php
+        echo JCckDev::renderForm( 'core_label', $this->item->label, $config );
+        echo JCckDev::renderForm( 'core_defaultvalue', $this->item->defaultvalue, $config );
+        echo JCckDev::renderForm( 'core_options_path', @$options2['path'], $config, array( 'required'=>'required' ) );
+		echo JCckDev::renderForm( 'core_selectlabel', $this->item->selectlabel, $config, array( 'defaultvalue'=>'Select a File' ) );
+		echo JCckDev::renderForm( 'core_bool2', $this->item->bool2, $config, array( 'label'=>'Recursively', 'defaultvalue'=>'0' ) );
+		echo JCckDev::renderForm( 'core_bool', $this->item->bool, $config, array( 'label'=>'Storage Format', 'options'=>'File Name or Path=1||Full Path=0' ) );
+
+		echo '<li><label>'.JText::_( 'COM_CCK_LEGAL_EXTENSIONS' ).'</label>'
+		 .	 JCckDev::getForm( 'core_options_media_extensions', $media_ext, $config )
+		 .	 JCckDev::getForm( 'core_options_legal_extensions', @$options2['legal_extensions'], $config, array( 'size'=>13, 'required'=>'required' ) )
+		 .	 '</li>';
+		echo JCckDev::renderBlank();
+		
+        echo JCckDev::renderSpacer( JText::_( 'COM_CCK_STORAGE' ), JText::_( 'COM_CCK_STORAGE_DESC' ) );
+		echo JCckDev::getForm( 'core_storage', $this->item->storage, $config );
+        ?>
+    </ul>
+</div>
+
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+	$('#json_options2_legal_extensions').isVisibleWhen('json_options2_media_extensions','custom',false);
+});
+</script>
