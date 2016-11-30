424511951e6de7472563f775d4254dbc70f551b7
Sebastien Heraud <sebastien@heraud.me>
2016-11-30 18:25:31 +0100

    Initial commit of Increment Restriction

---
+++ plugins/cck_field_restriction/increment/increment.php
@@ -0,0 +1,96 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_RestrictionIncrement extends JCckPluginRestriction
+{
+	protected static $type	=	'increment';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+
+	// onCCK_Field_RestrictionPrepareContent
+	public static function onCCK_Field_RestrictionPrepareContent( &$field, &$config )
+	{
+		if ( self::$type != $field->restriction ) {
+			return;
+		}
+		
+		$restriction	=	parent::g_getRestriction( $field->restriction_options );
+		
+		return self::_authorise( $restriction, $field, $config );
+	}
+
+	// onCCK_Field_RestrictionPrepareForm
+	public static function onCCK_Field_RestrictionPrepareForm( &$field, &$config )
+	{
+		if ( self::$type != $field->restriction ) {
+			return;
+		}
+		
+		$restriction	=	parent::g_getRestriction( $field->restriction_options );
+		
+		return self::_authorise( $restriction, $field, $config );
+	}
+	
+	// onCCK_Field_RestrictionPrepareStore
+	public static function onCCK_Field_RestrictionPrepareStore( &$field, &$config )
+	{
+		if ( self::$type != $field->restriction ) {
+			return;
+		}
+		
+		$restriction	=	parent::g_getRestriction( $field->restriction_options );
+		
+		return self::_authorise( $restriction, $field, $config );
+	}
+	
+	// _authorise
+	protected static function _authorise( $restriction, &$field, &$config )
+	{
+		$do				=	$restriction->get( 'do', 0 );
+		$execute		=	$restriction->get( 'execute', 0 );
+		$identifier		=	$restriction->get( 'identifier_name', '' );
+
+		if ( $identifier != '' ) {
+			require_once JPATH_SITE.'/plugins/cck_field_typo/joomla_jgrid/joomla_jgrid.php';
+
+			$i				=	plgCCK_Field_TypoJoomla_Jgrid::getStaticValue( $identifier );
+		} else {
+			static $i		=	0;
+			static $loaded	=	array();
+
+			$idx			=	$config['pk'];
+
+			if ( !isset( $loaded[$idx] ) ) {
+				$loaded[$idx]	=	++$i;
+			}
+		}
+
+		if ( $i == $execute ) {
+			$do	=	( $do ) ? false : true;
+		} else {
+			$do	=	( $do ) ? true : false;
+		}
+		
+		if ( $do ) {
+			return true;
+		} else {
+			$field->display	=	0;
+			$field->state	=	0;
+
+			return false;
+		}
+
+		return true;
+	}
+}
+?>
---
+++ plugins/cck_field_restriction/increment/tmpl/edit.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				http://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+JCckDev::initScript( 'restriction', $this->item );
+?>
+
+<div class="seblod">
+	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_RESTRICTION_'.$this->item->name.'_DESC' ) ); ?>
+    <ul class="adminformlist adminformlist-2cols">
+        <?php
+		echo JCckDev::renderForm( 'core_dev_bool', '', $config, array( 'label'=>'Execute', 'defaultvalue'=>'1', 'options'=>'1=1||2=2||3=3||4=4||5=5||6=6||7=7||8=8||9=9', 'storage_field'=>'execute' ) );
+		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Identifier', 'defaultvalue'=>'', 'storage_field'=>'identifier_name' ) );
+        ?>
+    </ul>
+</div>
