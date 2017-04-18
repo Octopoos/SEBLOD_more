4d405001d44f14530fd7e8ae2debd6813785502f
Sebastien Heraud <sebastien@heraud.me>
2017-04-18 09:54:13 +0200

    Update Copyright

--- plugins/cck_field_restriction/joomla_language/install/script.php
+++ plugins/cck_field_restriction/joomla_language/install/script.php
@@ -2,7 +2,7 @@
 /**
 * @version 			SEBLOD Multilingual 1.x
 * @package			SEBLOD Multilingual Add-on for SEBLOD 3.x
-* @url				http://www.seblod.com
+* @url				https://www.seblod.com
 * @editor			Octopoos - www.octopoos.com
 * @copyright		Copyright (C) 2009 - 2016 SEBLOD. All Rights Reserved.
 * @license 			GNU General Public License version 2 or later; see _LICENSE.php
