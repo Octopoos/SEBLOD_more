682e6466e78bcdbbb72e9c7d51cdaeb61a42ee72
Sebastien Heraud <sebastien@heraud.me>
2017-04-24 16:46:28 +0200

    Add missing "onCCK_Storage_LocationDelete" [Viewlevel Object Plug-in]

--- plugins/cck_storage_location/joomla_viewlevel/joomla_viewlevel.php
+++ plugins/cck_storage_location/joomla_viewlevel/joomla_viewlevel.php
@@ -178,9 +178,36 @@ class plgCCK_Storage_LocationJoomla_Viewlevel extends JCckPluginLocation
 	// onCCK_Storage_LocationDelete
 	public static function onCCK_Storage_LocationDelete( $pk, &$config = array() )
 	{
-		// TODO
+		$app		=	JFactory::getApplication();
+		$dispatcher	=	JEventDispatcher::getInstance();
+		$table		=	self::_getTable( $pk );	
 		
-		return false;
+		if ( !$table ) {
+			return false;
+		}
+		
+		// Check
+		$user 			=	JCck::getUser();
+		$canDelete		=	$user->authorise( 'core.delete', 'com_cck.form.'.$config['type_id'] );
+		$canDeleteOwn	=	$user->authorise( 'core.delete.own', 'com_cck.form.'.$config['type_id'] );
+		if ( ( !$canDelete && !$canDeleteOwn ) ||
+			 ( !$canDelete && $canDeleteOwn && $config['author'] != $user->id ) ||
+			 ( $canDelete && !$canDeleteOwn && $config['author'] == $user->id ) ) {
+			$app->enqueueMessage( JText::_( 'COM_CCK_ERROR_DELETE_NOT_PERMITTED' ), 'error' );
+			return;
+		}
+		
+		// Process
+		$result	=	$dispatcher->trigger( 'onContentBeforeDelete', array( self::$context, $table ) );
+		if ( in_array( false, $result, true ) ) {
+			return false;
+		}
+		if ( !$table->delete( $pk ) ) {
+			return false;
+		}
+		$dispatcher->trigger( 'onContentAfterDelete', array( self::$context, $table ) );
+		
+		return true;
 	}
 	
 	// onCCK_Storage_LocationStore
