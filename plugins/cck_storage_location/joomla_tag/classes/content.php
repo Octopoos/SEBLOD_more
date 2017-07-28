4a635e242df544d0a9b7da369adc203fd4ce605f
Sebastien Heraud <sebastien@heraud.me>
2017-07-28 16:12:50 +0200

    Update "JCckContentJoomla_Tag" Class for SEBLOD 3.12.0

--- plugins/cck_storage_location/joomla_tag/classes/content.php
+++ plugins/cck_storage_location/joomla_tag/classes/content.php
@@ -14,23 +14,23 @@ defined( '_JEXEC' ) or die;
 class JCckContentJoomla_Tag extends JCckContent
 {	
 	// preSave
-	public function preSave( $instance_name, &$data )
+	protected function preSave( $instance_name, &$data )
 	{
 		if ( $instance_name == 'base' ) {
 			if ( !isset( $data['parent_id'] ) ) {
-				$data['parent_id']	=	( $this->getPk() ) ? $this->{'_instance_'.$instance_name}->parent_id : 1;
+				$data['parent_id']	=	( $this->getPk() ) ? $this->_instance_base->parent_id : 1;
 			}
-			if ( !$this->getPk() || ( $data['parent_id'] != $this->{'_instance_'.$instance_name}->parent_id ) ) {
-				$this->{'_instance_'.$instance_name}->setLocation( $data['parent_id'], 'last-child' );
+			if ( !$this->getPk() || ( $data['parent_id'] != $this->_instance_base->parent_id ) ) {
+				$this->_instance_base->setLocation( $data['parent_id'], 'last-child' );
 			}
 		}
 	}
 
 	// postSave
-	public function postSave( $instance_name, $data )
+	protected function postSave( $instance_name, $data )
 	{
 		if ( $instance_name == 'base' ) {
-			$this->{'_instance_'.$instance_name}->rebuildPath( $this->getPk() );
+			$this->_instance_base->rebuildPath( $this->getPk() );
 		}
 	}
 }
