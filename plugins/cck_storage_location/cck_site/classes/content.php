f42e1363abb226912d0539e2cd60374ae0542f4d
Sebastien Heraud <sebastien@heraud.me>
2017-09-12 09:35:20 +0200

    Implement preSave on JCckContentCck_Site [Site Object Plug-in]

--- plugins/cck_storage_location/cck_site/classes/content.php
+++ plugins/cck_storage_location/cck_site/classes/content.php
@@ -13,5 +13,61 @@ defined( '_JEXEC' ) or die;
 // JCckContent
 class JCckContentCck_Site extends JCckContent
 {
+	// preSave
+	protected function preSave( $instance_name, &$data )
+	{
+		if ( $instance_name == 'base' && $this->isNew() ) {
+			$config		=	array(
+							'isNew'=>true,
+							'storages'=>array( '#__cck_core_sites'=>array() ),
+							'type'=>$this->getType()
+						);
+			$fields		=	array();
+
+			if ( isset( $data['_'] ) ) {
+				foreach ( $data['_'] as $k=>$v ) {
+					$fields[$k]	=	new stdClass;
+					
+					$fields[$k]->value	=	$v;
+				}
+
+				unset( $data['_'] );
+			}
+
+			$groups	=	$this->_preSave_onCckPostBeforeStore( $config, $fields );
+
+			if ( $groups ) {
+				$data['groups']	=	$groups;
+			}
+
+			unset( $config );
+			unset( $fields );
+		}
+	}
+
+	// _preSave_onCckPostBeforeStore
+	private function _preSave_onCckPostBeforeStore( $config, $fields )
+	{
+		if ( !JCckToolbox::getConfig()->get( 'processing', 0 ) ) {
+			return '';
+		}
+
+		$event		=	'onCckPostBeforeStore';
+		$processing =	JCckDatabaseCache::loadObjectListArray( 'SELECT type, scriptfile, options FROM #__cck_more_processings WHERE published = 1 ORDER BY ordering', 'type' );
+
+		if ( isset( $processing[$event] ) ) {
+			foreach ( $processing[$event] as $p ) {
+				if ( is_file( JPATH_SITE.$p->scriptfile ) ) {
+					$options	=	new JRegistry( $p->options );
+
+					include_once JPATH_SITE.$p->scriptfile; /* Variables: $fields, $config, $user */
+				}
+			}
+		}
+
+		if ( isset( $config['storages']['#__cck_core_sites']['groups'] ) ) {
+			return $config['storages']['#__cck_core_sites']['groups'];
+		}
+	}
 }
 ?>
