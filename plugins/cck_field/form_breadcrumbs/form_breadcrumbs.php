9e92473665c98d9d7c6ffda1dc24bc3ad732da4a
Sebastien Heraud <sebastien@heraud.me>
2018-01-16 16:01:08 +0100

    Initial commit of Form Breadcrumbs Plug-in

---
+++ plugins/cck_field/form_breadcrumbs/form_breadcrumbs.php
@@ -0,0 +1,181 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				https://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2017 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_FieldForm_Breadcrumbs extends JCckPluginField
+{
+	protected static $type		=	'form_breadcrumbs';
+	protected static $path;
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Construct
+	
+	// onCCK_FieldConstruct
+	public function onCCK_FieldConstruct( $type, &$data = array() )
+	{
+		if ( self::$type != $type ) {
+			return;
+		}
+		parent::g_onCCK_FieldConstruct( $data );
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+	
+	// onCCK_FieldPrepareContent
+	public function onCCK_FieldPrepareContent( &$field, $value = '', &$config = array() )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		parent::g_onCCK_FieldPrepareContent( $field, $config );
+		
+		// Set
+		$field->value	=	$value;
+	}
+	
+	// onCCK_FieldPrepareForm
+	public function onCCK_FieldPrepareForm( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		self::$path	=	parent::g_getPath( self::$type.'/' );
+		parent::g_onCCK_FieldPrepareForm( $field, $config );
+		
+		// Init
+		if ( count( $inherit ) ) {
+			$id		=	( isset( $inherit['id'] ) && $inherit['id'] != '' ) ? $inherit['id'] : $field->name;
+			$name	=	( isset( $inherit['name'] ) && $inherit['name'] != '' ) ? $inherit['name'] : $field->name;
+		} else {
+			$id		=	$field->name;
+			$name	=	$field->name;
+		}
+		$form		=	'';
+		$value		=	'';
+
+		// Prepare
+		parent::g_addProcess( 'beforeRenderForm', self::$type, $config, array( 'id'=>$id, 'name'=>$name, 'child_names'=>$field->options ) );
+		
+		// Set
+		if ( ! $field->variation ) {
+			$field->form	=	$form;
+			if ( $field->script ) {
+				parent::g_addScriptDeclaration( $field->script );
+			}
+		} else {
+			parent::g_getDisplayVariation( $field, $field->variation, $value, $value, $form, $id, $name, '<input', '', '', $config );
+		}
+
+		$field->value	=	$value;
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
+	}
+	
+	// onCCK_FieldPrepareSearch
+	public function onCCK_FieldPrepareSearch( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		
+		// Prepare
+		self::onCCK_FieldPrepareForm( $field, $value, $config, $inherit, $return );
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
+	}
+	
+	// onCCK_FieldPrepareStore
+	public function onCCK_FieldPrepareStore( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		
+		// Init
+		if ( count( $inherit ) ) {
+			$name	=	( isset( $inherit['name'] ) && $inherit['name'] != '' ) ? $inherit['name'] : $field->name;
+		} else {
+			$name	=	$field->name;
+		}
+		
+		// Validate
+		parent::g_onCCK_FieldPrepareStore_Validation( $field, $name, $value, $config );
+		
+		// Set or Return
+		if ( $return === true ) {
+			return $value;
+		}
+		$field->value	=	$value;
+		parent::g_onCCK_FieldPrepareStore( $field, $name, $value, $config );
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Render
+	
+	// onCCK_FieldRenderContent
+	public static function onCCK_FieldRenderContent( $field, &$config = array() )
+	{
+		return parent::g_onCCK_FieldRenderContent( $field );
+	}
+	
+	// onCCK_FieldRenderForm
+	public static function onCCK_FieldRenderForm( $field, &$config = array() )
+	{
+		return parent::g_onCCK_FieldRenderForm( $field );
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Special Events
+	
+	// onCCK_FieldBeforeRenderForm
+	public static function onCCK_FieldBeforeRenderForm( $process, &$fields, &$storages, &$config = array() )
+	{
+		$app				=	JFactory::getApplication();
+		$name				=	$process['name'];
+
+		$class				=	( $fields[$name]->css ) ? ' class="'.$fields[$name]->css.'"' : '';
+		$children			=	explode( '||', $process['child_names'] );
+		$form				=	'';
+		$stage				=	(int)$app->input->getInt( 'stage' );
+
+		if ( $stage > 1 ) {
+			$stage--;
+		}
+
+		if ( count( $children ) ) {
+			$doTranslation	=	$config['doTranslation'];
+
+			foreach ( $children as $k=>$child ) {
+				$css	=	'';
+				$text	=	$child;
+
+				if ( $k == $stage ) {
+					$css	=	' class="active"';
+				}
+				if ( $doTranslation ) {
+					$text	=	JText::_( 'COM_CCK_' . str_replace( ' ', '_', trim( $text ) ) );
+				}
+
+				$form	.=	'<li'.$css.'>'.$text.'</li>';
+			}
+		}
+		if ( $form ) {
+			$form	=	'<ul'.$class.'>'.$form.'</ul>';
+		}
+
+		$fields[$name]->form	=	$form;
+	}
+}
+?>
---
+++ plugins/cck_field/form_breadcrumbs/tmpl/edit.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				https://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2017 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+JCckDev::forceStorage();
+$options    =   JCckDev::fromSTRING( $this->item->options );
+?>
+
+<div class="seblod">
+    <?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_'.$this->item->type.'_DESC' ) ); ?>
+    <ul class="adminformlist adminformlist-2cols">
+    <?php
+    echo JCckDev::renderForm( 'core_label', $this->item->label, $config );
+    echo JCckDev::renderBlank();
+    echo JCckDev::renderForm( 'core_options', $options, $config, array( 'label'=>'Stages', 'rows'=>1 ) );
+    
+    echo JCckDev::renderSpacer( JText::_( 'COM_CCK_STORAGE' ), JText::_( 'COM_CCK_STORAGE_DESC' ) );
+    echo JCckDev::getForm( 'core_storage', $this->item->storage, $config );
+    ?>
+    </ul>
+</div>
