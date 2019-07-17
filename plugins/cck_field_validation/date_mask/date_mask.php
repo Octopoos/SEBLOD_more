dd77387ba5c231eb98daee7385facfa6b34ee8b9
Sebastien Heraud <sebastien@heraud.me>
2019-07-17 11:46:04 +0200

    Initial commit of Date Mask Validation Plug-in

---
+++ plugins/cck_field_validation/date_mask/date_mask.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				https://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2018 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_ValidationDate_Mask extends JCckPluginValidation
+{
+	protected static $type	=	'date_mask';
+	protected static $regex	=	'/^[a-zA-Z]+$/';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+
+	// onCCK_Field_ValidationPrepareForm
+	public static function onCCK_Field_ValidationPrepareForm( &$field, $fieldId, &$config )
+	{
+		if ( self::$type != $field->validation ) {
+			return;
+		}
+		
+		// $field->attributes	.=	' data-inputmask-alias="datetime" data-inputmask-inputformat="yyyy-mm-dd"';
+		$field->attributes	.=	' data-inputmask="\'alias\':\'datetime\', \'inputformat\':\'yyyy-mm-dd\'"';
+
+		JFactory::getDocument()->addScript( JUri::root( true ).'/media/cck/js/jquery.inputmask.min.js' );
+		JFactory::getDocument()->addScript( JUri::root( true ).'/media/cck/js/jquery.inputmask.binding.min.js' );
+	}
+	
+	// onCCK_Field_ValidationPrepareStore
+	public static function onCCK_Field_ValidationPrepareStore( &$field, $name, $value, &$config )
+	{
+		/* TODO */
+	}
+}
+?>
