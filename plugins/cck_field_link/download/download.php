8027b3a72a63d42226a53a2b539a4267fc567769
Sebastien Heraud <sebastien@heraud.me>
2020-11-30 16:27:41 +0100

    Improve Download Link Plug-in

--- plugins/cck_field_link/download/download.php
+++ plugins/cck_field_link/download/download.php
@@ -38,9 +38,19 @@ class plgCCK_Field_LinkDownload extends JCckPluginLink
 		// Init
 		$content			=	$link->get( 'content', '' );
 		$content_fieldname	=	$link->get( 'content_fieldname', '' );
+		$file_client		=	$link->get( 'file_client', '0' );
 		$file_fieldname		=	$link->get( 'file_fieldname', '' );
+		$type				=	$link->get( 'type', 'download' );
+
+		$link_attr			=	$link->get( 'attributes', '' );
 		$link_class			=	$link->get( 'class', '' );
-		$link_more			=	( $config['client'] == 'intro' /*|| $config['client'] == 'list' || $config['client'] == 'item'*/ ) ? '&client='.$config['client'] : '';
+		$link_rel			=	$link->get( 'rel', '' );
+		$link_target		=	$link->get( 'target', '' );
+		$link_more			=	'';
+
+		if ( $file_client ) {
+			$link_more			=	( $config['client'] == 'intro' /*|| $config['client'] == 'list' || $config['client'] == 'item'*/ ) ? '&client='.$config['client'] : '';
+		}
 		$link_title			=	$link->get( 'title', '' );
 		$link_title2		=	$link->get( 'title_custom', '' );
 		$xi					=	0;
@@ -57,7 +67,7 @@ class plgCCK_Field_LinkDownload extends JCckPluginLink
 				$field->hits	=	(int)JCckDatabase::loadResult( $query ); //@
 				
 				$link_more2		=	$link_more.'&collection='.$collection.'&xi='.$xi;
-				$f->link		=	JRoute::_( 'index.php?option=com_cck&task=download'.$link_more2.'&file='.$f->name.'&id='.$config['id'].$itemId );
+				$f->link		=	JRoute::_( 'index.php?option=com_cck&task='.$type.$link_more2.'&file='.$f->name.'&id='.$config['id'].$itemId );
 				$f->link_class	=	$link_class ? $link_class : ( isset( $f->link_class ) ? $f->link_class : '' );
 
 				if ( $link_title ) {
@@ -85,14 +95,16 @@ class plgCCK_Field_LinkDownload extends JCckPluginLink
 				$field->link	=	'';
 				$pk				=	0;
 				
-				parent::g_addProcess( 'beforeRenderContent', self::$type, $config, array( 'name'=>$field->name, 'content_fieldname'=>$content_fieldname, 'file_fieldname'=>$field_name, 'link_more'=>$link_more, 'pk'=>$pk ) );
+				parent::g_addProcess( 'beforeRenderContent', self::$type, $config, array( 'name'=>$field->name, 'content_fieldname'=>$content_fieldname, 'file_fieldname'=>$field_name, 'link_more'=>$link_more, 'pk'=>$pk, 'type'=>$type ) );
 			} else {
 				$query			=	'SELECT a.hits FROM #__cck_core_downloads AS a WHERE a.id = '.(int)$pk.' AND a.field = "'.(string)$field_name.'" AND a.collection = "'.(string)$collection.'" AND a.x = '.(int)$xi;
 				$field->hits	=	(int)JCckDatabase::loadResult( $query ); //@
-				$field->link	=	JRoute::_( 'index.php?option=com_cck&task=download'.$link_more.'&file='.$file_fieldname.'&id='.$pk.$itemId );
+				$field->link	=	JRoute::_( 'index.php?option=com_cck&task='.$type.$link_more.'&file='.$file_fieldname.'&id='.$pk.$itemId );
 			}
 
-			$field->link_class	=	$link_class ? $link_class : ( isset( $field->link_class ) ? $field->link_class : '' );
+			$field->link_attributes	=	$link_attr ? $link_attr : ( isset( $field->link_attributes ) ? $field->link_attributes : '' );
+			$field->link_class		=	$link_class ? $link_class : ( isset( $field->link_class ) ? $field->link_class : '' );
+			$field->link_target		=	$link_target ? ( $link_target == 'modal' ? '' : $link_target ) : ( isset( $field->link_target ) ? $field->link_target : '' );
 
 			if ( $link_title ) {
 				if ( $link_title == '2' ) {
@@ -117,7 +129,7 @@ class plgCCK_Field_LinkDownload extends JCckPluginLink
 		$pk			=	isset( $fields[$fieldname] ) ? (int)$fields[$fieldname]->value : $process['pk'];
 
 		if ( $pk ) {
-			$fields[$name]->link	=	JRoute::_( 'index.php?option=com_cck&task=download'.$process['link_more'].'&file='.$process['file_fieldname'].'&id='.$pk.$itemId );
+			$fields[$name]->link	=	JRoute::_( 'index.php?option=com_cck&task='.$process['type'].$process['link_more'].'&file='.$process['file_fieldname'].'&id='.$pk.$itemId );
 			$target					=	 $fields[$name]->typo_target;
 
 			if ( isset( $fields[$name]->typo_mode ) && $fields[$name]->typo_mode ) {
--- plugins/cck_field_link/download/tmpl/edit.php
+++ plugins/cck_field_link/download/tmpl/edit.php
@@ -17,13 +17,17 @@ JCckDev::initScript( 'link', $this->item );
 	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_LINK_'.$this->item->name.'_DESC' ) ); ?>
     <ul class="adminformlist adminformlist-2cols">
         <?php
+        echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'label'=>'Task', 'selectlabel'=>'', 'defaultvalue'=>'download', 'options'=>'Download=download||Read=read', 'storage_field'=>'type', 'required'=>'required' ) );
 		echo JCckDev::renderForm( 'core_dev_select', '', $config, array( 'defaultvalue'=>'', 'label'=>'Content', 'selectlabel'=>'Current', 'options'=>'Use Value=optgroup||Field=2', 'storage_field'=>'content' ) );
 		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Field Name', 'storage_field'=>'file_fieldname' ) );
 		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Field Name', 'storage_field'=>'content_fieldname' ) );
 		echo JCckDev::renderForm( 'core_menuitem', '', $config, array( 'selectlabel'=>'None' ) );
+		echo JCckDev::renderForm( 'core_bool', '', $config, array( 'defaultvalue'=>'0', 'label'=>'Client', 'storage_field'=>'file_client' ) );
 
 		echo JCckDev::renderSpacer( JText::_( 'COM_CCK_CONSTRUCTION' ) . '<span class="mini">('.JText::_( 'COM_CCK_GENERIC' ).')</span>' );
+		echo JCckDev::renderForm( 'core_attributes', '', $config, array( 'label'=>'Custom Attributes', 'storage_field'=>'attributes' ), array(), 'w100' );
 		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Class', 'size'=>24, 'storage_field'=>'class' ) );
+		echo JCckDev::renderForm( 'core_options_target', '', $config, array( 'defaultvalue'=>'', 'selectlabel'=>'Inherited', 'options'=>'Target Blank=_blank||Target Self=_self||Target Parent=_parent||Target Top=_top||Advanced=optgroup', 'storage_field'=>'target' ) );
 		echo '<li><label>'.JText::_( 'COM_CCK_TITLE' ).'</label>'
 			. JCckDev::getForm( 'core_dev_select', '', $config, array( 'selectlabel'=>'None', 'options'=>'Custom Text=2||Translated Text=3', 'storage_field'=>'title' ) )
 			. JCckDev::getForm( 'core_dev_text', '', $config, array( 'label'=>'Title', 'size'=>16, 'css'=>'input-medium', 'storage_field'=>'title_custom' ) )
