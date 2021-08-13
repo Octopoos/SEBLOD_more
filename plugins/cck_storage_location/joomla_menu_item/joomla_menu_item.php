560037f0a2ddfe6f5550e4e52a5ad2c9e1f5064e
Sebastien Heraud <sebastien@heraud.me>
2021-08-13 18:36:38 +0200

    Add "$data" parameter on "onContentBeforeSave" [Joomla! Menu Item Object]

--- plugins/cck_storage_location/joomla_menu_item/joomla_menu_item.php
+++ plugins/cck_storage_location/joomla_menu_item/joomla_menu_item.php
@@ -340,7 +340,7 @@ class plgCCK_Storage_LocationJoomla_Menu_Item extends JCckPluginLocation
 		self::_completeTable( $table, $data, $config );
 
 		// Store
-		$app->triggerEvent( 'onContentBeforeSave', array( self::$context, &$table, $isNew ) );
+		$app->triggerEvent( 'onContentBeforeSave', array( self::$context, &$table, $isNew, $data ) );
         if ( !$table->store() ) {
 			JFactory::getApplication()->enqueueMessage( $table->getError(), 'error' );
 
