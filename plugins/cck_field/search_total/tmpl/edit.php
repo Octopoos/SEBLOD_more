98ccf26a3f629e61eb117104c62b303ca9bdad0e
Sebastien Heraud <seb@heraud.me>
2022-07-25 09:25:24 +0200

    Prepare Search Total 1.5.0 Release (J! 4x)

--- plugins/cck_field/search_total/tmpl/edit.php
+++ plugins/cck_field/search_total/tmpl/edit.php
@@ -10,22 +10,35 @@
 
 defined( '_JEXEC' ) or die;
 
+// Init
 JCckDev::forceStorage();
+
 $options2       =   JCckDev::fromJSON( $this->item->options2 );
 $defaultvalue   =   ( !isset( $options2['jtext'] ) ) ? '1' : '0';
-?>
 
-<div class="seblod">
-	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_'.$this->item->type.'_DESC' ) ); ?>
-    <ul class="adminformlist adminformlist-2cols">
-        <?php
-        echo JCckDev::renderForm( 'core_label', $this->item->label, $config );
-        echo JCckDev::renderForm( 'core_defaultvalue', $this->item->defaultvalue, $config );
-		echo JCckDev::renderForm( 'core_dev_text', @$options2['jtext'], $config, array( 'label'=>'Language Constant', 'defaultvalue'=>'COM_CCK_SEARCH_TOTAL', 'storage_field'=>'json[options2][jtext]' ) );
-        echo JCckDev::renderForm( 'core_dev_bool', @$options2['alternative'], $config, array( 'label'=>'Use Alternative', 'defaultvalue'=>$defaultvalue, 'storage_field'=>'json[options2][alternative]' ) );
-		
-		echo JCckDev::renderSpacer( JText::_( 'COM_CCK_STORAGE' ), JText::_( 'COM_CCK_STORAGE_DESC' ) );
-		echo JCckDev::getForm( 'core_storage', $this->item->storage, $config );
-        ?>
-    </ul>
-</div>
+// Set
+$displayData    =   array(
+                        'config'=>$config,
+                        'form'=>array(
+                            array(
+                                'fields'=>array(
+                                    JCckDev::renderForm( 'core_label', $this->item->label, $config ),
+                                    JCckDev::renderForm( 'core_defaultvalue', $this->item->defaultvalue, $config ),
+                                    JCckDev::renderForm( 'core_dev_text', @$options2['jtext'], $config, array( 'label'=>'Language Constant', 'defaultvalue'=>'COM_CCK_SEARCH_TOTAL', 'storage_field'=>'json[options2][jtext]' ) ),
+                                    JCckDev::renderForm( 'core_dev_bool', @$options2['alternative'], $config, array( 'label'=>'Use Alternative', 'defaultvalue'=>$defaultvalue, 'storage_field'=>'json[options2][alternative]' ) )
+                                )
+                            ),
+                            array(
+                                'fields'=>array(
+                                    JCckDev::getForm( 'core_storage', $this->item->storage, $config )
+                                ),
+                                'mode'=>'storage'
+                            )
+                        ),
+                        'html'=>'',
+                        'item'=>$this->item,
+                        'script'=>''
+                    );
+
+echo JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.cck_field.edit', $displayData );
+?>
