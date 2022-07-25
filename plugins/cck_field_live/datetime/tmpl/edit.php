529f69e64b932c7c39b29939c840dfa18461d370
Sebastien Heraud <seb@heraud.me>
2022-07-25 09:25:11 +0200

    Prepare DateTime Live 1.3.0 Release (J! 4x)

--- plugins/cck_field_live/datetime/tmpl/edit.php
+++ plugins/cck_field_live/datetime/tmpl/edit.php
@@ -10,28 +10,32 @@
 
 defined( '_JEXEC' ) or die;
 
+// JS
+$js =	'jQuery(document).ready(function($) {
+			$("#format_custom").isVisibleWhen("format","-1");
+		});';
+
+// Set
+echo JCckDev::renderLayoutFile( 'cck'.JCck::v().'.construction.admin.edit', array(
+	'config'=>$config,
+	'form'=>array(
+		array(
+			'fields'=>array(
+				JCckDev::renderForm( 'core_dev_select', 'Y-m-d', $config, array( 'label'=>'Format', 'defaultvalue'=>'', 'selectlabel'=>'Auto',
+									  'options'=>'Free=-1||Use JText=optgroup||DATE_FORMAT_LC=DATE_FORMAT_LC||DATE_FORMAT_LC1=DATE_FORMAT_LC1||DATE_FORMAT_LC2=DATE_FORMAT_LC2||DATE_FORMAT_LC3=DATE_FORMAT_LC3||DATE_FORMAT_LC4=DATE_FORMAT_LC4||DATE_FORMAT_JS1=DATE_FORMAT_JS1||DATE_FORMAT_TZ=DATE_FORMAT_TZ',
+									  'storage_field'=>'format' ) ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Free Format', 'storage_field'=>'format_custom' ) ),
+				JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Modifier', 'storage_field'=>'modify' ) ),
+				JCckDev::renderForm( 'core_bool', '', $config, array( 'label'=>'Apply Time Zone', 'defaultvalue'=>'0', 'storage_field'=>'timezone' ) ),
+				JCckDev::renderForm( 'core_dev_textarea', '', $config, array( 'label'=>'Return JText', 'cols'=>50, 'rows'=>1, 'storage_field'=>'return_jtext' ), array(), 'w100' )
+			)
+		)
+	),
+	'html'=>'',
+	'item'=>$this->item,
+	'script'=>$js,
+	'type'=>'live'
+) );
+
 JCckDev::initScript( 'live', $this->item );
 ?>
-
-<div class="seblod">
-	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_LIVE_'.$this->item->name.'_DESC' ) ); ?>
-    <ul class="adminformlist adminformlist-2cols">
-        <?php
-		echo JCckDev::renderForm( 'core_dev_select', 'Y-m-d', $config, array( 'label'=>'Format', 'defaultvalue'=>'', 'selectlabel'=>'Auto',
-								  'options'=>'Free=-1||Use JText=optgroup||DATE_FORMAT_LC=DATE_FORMAT_LC||DATE_FORMAT_LC1=DATE_FORMAT_LC1||DATE_FORMAT_LC2=DATE_FORMAT_LC2||DATE_FORMAT_LC3=DATE_FORMAT_LC3||DATE_FORMAT_LC4=DATE_FORMAT_LC4||DATE_FORMAT_JS1=DATE_FORMAT_JS1||DATE_FORMAT_TZ=DATE_FORMAT_TZ',
-								  'storage_field'=>'format' ) );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Free Format', 'storage_field'=>'format_custom' ) );
-		echo JCckDev::renderForm( 'core_dev_text', '', $config, array( 'label'=>'Modifier', 'storage_field'=>'modify' ) );
-		echo JCckDev::renderForm( 'core_bool', '', $config, array( 'label'=>'Apply Time Zone', 'defaultvalue'=>'0', 'storage_field'=>'timezone' ) );
-
-		echo JCckDev::renderSpacer( JText::_( 'COM_CCK_CONSTRUCTION' ) . '<span class="mini">('.JText::_( 'COM_CCK_GENERIC' ).')</span>' );
-		echo JCckDev::renderForm( 'core_dev_textarea', '', $config, array( 'label'=>'Return JText', 'cols'=>50, 'rows'=>1, 'storage_field'=>'return_jtext' ), array(), 'w100' );
-		?>
-    </ul>
-</div>
-
-<script type="text/javascript">
-jQuery(document).ready(function($) {
-	$('#format_custom').isVisibleWhen('format','-1');
-});
-</script>
